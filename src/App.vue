13553a2216341d0892b097484906d497762df87e
Pedro Hasse Niemczewski <pedro.hasse@dtidigital.com.br>
2025-09-11 12:49:47 -0300

    feat: instalação e configuração inicial do vuetify 3 e do vue router

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-main class="fill-height">
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style lang="scss">
+@import 'vuetify/styles';
+
+html, body, #app {
+  height: 100%;
+  width: 100%;
+  padding: 0;
+  margin: 0;
+}
+
+.v-application {
+  min-height: 100vh;
+}
+</style>
