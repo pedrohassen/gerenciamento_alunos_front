f8b14f91877be900911a2ade774137a0bbfbd425
Pedro Hasse Niemczewski <pedro.hasse@dtidigital.com.br>
2025-09-17 12:28:25 -0300

    refactor: agrupa átomo texto e átomo link em uma só molécula

---
+++ src/components/atomicDesign/molecule/MoleculaTextoLink.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="d-flex align-center justify-center w-100">
+    <AtomoTexto tag="span" class="tamanho-fonte">{{ text }}</AtomoTexto>
+    <AtomoLink :to="to" class="ml-2 tamanho-fonte">{{ linkText }}</AtomoLink>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import AtomoTexto from "../atom/AtomoTexto.vue";
+import AtomoLink from "../atom/AtomoLink.vue";
+
+export default defineComponent({
+  name: "MoleculaTextoLink",
+  components: { AtomoTexto, AtomoLink },
+  props: {
+    text: { type: String, required: true },
+    linkText: { type: String, required: true },
+    to: { type: String, required: true },
+  },
+});
+</script>
+
+<style scoped>
+.tamanho-fonte {
+  font-size: 18px !important;
+}
+</style>
