5036755f79a45b4cee9288487859c5fe2705b478
Pedro Hasse Niemczewski <pedro.hasse@dtidigital.com.br>
2025-09-19 12:55:50 -0300

    feat: reoganização das views em conjunto com o router-view para renderização correta de cada view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,17 @@
 <template>
-  <v-app>
-    <v-main>
-      <v-container>
-        <AtomoTexto class="text-h4">Bem-vindo ao Gerenciamento de Alunos!</AtomoTexto>
-      </v-container>
-    </v-main>
-  </v-app>
+  <AtomoTexto tag="span" size="20px" weight="600" color="#000" class="mt-16">
+    Bem vindo!
+  </AtomoTexto>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import OrganismoLayoutPrincipal from '../components/atomicDesign/organism/OrganismoLayoutPrincipal.vue';
 import AtomoTexto from '../components/atomicDesign/atom/AtomoTexto.vue';
 
 export default defineComponent({
   components: {
+    OrganismoLayoutPrincipal,
     AtomoTexto
   }
 });
