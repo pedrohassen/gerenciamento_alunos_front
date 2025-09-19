d13208c58227701ff10b20efa22626b78333f265
Pedro Hasse Niemczewski <pedro.hasse@dtidigital.com.br>
2025-09-19 12:45:48 -0300

    feat: criação de componentes que criam o menu superior e lateral

---
+++ src/components/atomicDesign/molecule/MoleculaMenuLateral.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-navigation-drawer
+    v-model="open"
+    temporary
+    :width="$vuetify.display.smAndUp ? 200 : 300"
+    :clipped="$vuetify.display.smAndUp"
+  >
+    <v-list dense class="menu-list">
+      <v-list-item v-for="item in itens" :key="item.text" @click="$emit('navigate', item.route)" class="menu-item">
+        <v-list-item-icon>
+          <AtomoIcone :nome="item.icon || 'help_outline'" tamanho="20" cor="#000" />
+        </v-list-item-icon>
+        <v-list-item-content class="ml-3">
+          <AtomoTexto tag="span" size="14px" weight="500">
+            {{ item.text }}
+          </AtomoTexto>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, watch, type PropType } from 'vue';
+import AtomoTexto from '../atom/AtomoTexto.vue';
+import AtomoIcone from '../atom/AtomoIcone.vue';
+import type { MenuItem } from '../../../utils/types/menu';
+
+export default defineComponent({
+  name: 'MoleculaMenuLateral',
+  components: { AtomoTexto, AtomoIcone },
+  props: {
+    modelValue: {
+      type: Boolean,
+      required: true,
+    },
+    itens: {
+      type: Array as PropType<MenuItem[]>,
+      default: () => [],
+      required: true,
+    },
+  },
+  emits: ['navigate', 'update:modelValue'],
+  setup(props, { emit }) {
+    const open = ref(props.modelValue);
+
+    watch(open, (val) => emit('update:modelValue', val));
+    watch(() => props.modelValue, (val) => open.value = val);
+
+    return { open };
+  }
+});
+</script>
+
+<style scoped>
+.menu-list {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.menu-item {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  width: 100%;
+}
+</style>
---
+++ src/components/atomicDesign/molecule/MoleculaMenuSuperior.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-app-bar app color="primary" dark elevate-on-scroll>
+    <AtomoButton
+      buttonColor="transparent"
+      :style="{ minWidth: '40px', width: '40px', height: '40px', padding: 0 }"
+      @click="$emit('toggleDrawer')"
+    >
+      <AtomoIcone nome="menu" cor="#fff" tamanho="24" />
+    </AtomoButton>
+
+    <v-toolbar-title>
+      <AtomoTexto tag="h1" size="18px" weight="600" color="#fff">
+        {{ titulo }}
+      </AtomoTexto>
+    </v-toolbar-title>
+    <v-spacer />
+
+    <v-toolbar-items class="actions-container">
+      <slot name="actions" />
+    </v-toolbar-items>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import AtomoTexto from '../atom/AtomoTexto.vue';
+import AtomoIcone from '../atom/AtomoIcone.vue';
+import AtomoButton from '../atom/AtomoButton.vue';
+
+export default defineComponent({
+  name: 'MoleculaMenuSuperior',
+  components: { AtomoTexto, AtomoButton, AtomoIcone },
+  props: {
+    titulo: {
+      type: String,
+      default: 'Meu App',
+      required: true,
+    },
+  },
+  emits: ['toggleDrawer'],
+});
+</script>
+
+<style scoped>
+.actions-container {
+  display: flex;
+  font-weight: 600;
+  align-items: center;
+  padding-right: 100px;
+  gap: 12px;
+}
+</style>
